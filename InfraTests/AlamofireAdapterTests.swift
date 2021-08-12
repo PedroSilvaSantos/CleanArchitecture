60773cd4182556e85fe9670fd6e56c12bb98ed0e
Pedro Silva <pedro.osilva@mercadolivre.com>
2021-08-12 00:19:48 -0300

    add  infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Pedro Silva Dos Santos on 12/08/21.
+//
+
+import XCTest
+
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() throws {
+
+    }
+}
