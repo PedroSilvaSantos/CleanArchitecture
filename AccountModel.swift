29aad1d83e1daf10c873ce80b29c07ee1e045d2e
Pedro Silva <pedro.dossantos@castgroup.com.br>
2023-10-05 19:16:47 -0300

    Add account use case

---
+++ AccountModel.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+//Caso retorne com sucesso, a aplicacao devolverá uma conta contendo esses dados populados
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
---
+++ AddAccount.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+
+//Em caso de sucesso ele develverá um AccountModel e caso de error deveolverar o Error
+//Nao retorna nada - Void
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+    
+    
+}
+
+
+//Operacao assincrona - temos que aguardar um servico responder
+//A api espera receber esses dados
+struct AddAccountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
