d29aa9322d746fe70c1a30badc039b7d891042a5
Pedro Silva <pedro.dossantos@castgroup.com.br>
2023-10-05 22:37:28 -0300

    transform domain use case and models to public

--- Domain/AccountModel.swift
+++ Domain/AccountModel.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 //Caso retorne com sucesso, a aplicacao devolverá uma conta contendo esses dados populados
-struct AccountModel {
-    var id: String
-    var name: String
-    var email: String
-    var password: String
+public struct AccountModel {
+    public var id: String
+    public var name: String
+    public var email: String
+    public var password: String
 }
--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //Em caso de sucesso ele develverá um AccountModel e caso de error deveolverar o Error
 //Nao retorna nada - Void
-protocol AddAccount {
+public protocol AddAccount {
     func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
 
 }
@@ -10,9 +10,9 @@ protocol AddAccount {
 
 //Operacao assincrona - temos que aguardar um servico responder
 //A api espera receber esses dados
-struct AddAccountModel {
-    var name: String
-    var email: String
-    var password: String
-    var passwordConfirmation: String
+public struct AddAccountModel {
+    public var name: String
+    public var email: String
+    public var password: String
+    public var passwordConfirmation: String
 }
