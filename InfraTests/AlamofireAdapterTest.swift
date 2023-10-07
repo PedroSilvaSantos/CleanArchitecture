99fd1895ad13062a284d331e0fcaae6aeab696be
Pedro Silva <pedro.dossantos@castgroup.com.br>
2023-10-07 12:19:07 -0300

    ensure AlamofireAdapter calls Alamofire with correct URL

--- InfraTests/AlamofireAdapterTest.swift
+++ InfraTests/AlamofireAdapterTest.swift
@@ -10,7 +10,7 @@ class AlamofireAdapter {
     }
     
     public func post(to url: URL) {
-
+        session.request(url).resume()
     }
 }
 
@@ -24,12 +24,31 @@ final class AlamofireAdapterTest: XCTestCase {
         let session = Session(configuration: configuration)
         let sut = AlamofireAdapter(session: session)
         sut.post(to: url)
+        //teste assincrono, devemos usar o expectation
+        let exp = expectation(description: "waiting")
+        URLProtocolStub.observerRequest { request in
+            XCTAssertEqual(url, request.url)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 
 
 //URLProtocol é uma classe
+//intercepta URL session, alamofire e qlq outro
 class URLProtocolStub: URLProtocol {
+    
+    //variavel responsavel por guardar o valor retornado no request
+    //designer patter Observation
+    static var emit: ((URLRequest) -> Void)?
+    
+    //precisamos trabalhar com metodos staticos para termos referencia do lado de fora da classe
+    //observando o retorno do request, podendo ser chamado do lado de fora da classe
+    static func observerRequest(completion: @escaping (URLRequest) -> Void) {
+        URLProtocolStub.emit = completion
+    }
+    //metodos staticos
     override open class func canInit(with request: URLRequest) -> Bool {
         //true - vamos interceptar todos os request
         return true
@@ -39,10 +58,10 @@ class URLProtocolStub: URLProtocol {
         return request
     }
 
+    //metodos de instancia
     override open func startLoading() {
         //toda logica ficará aqui, simiular com error, falha e sucesso
-        
+        URLProtocolStub.emit?(request)
     }
-
     override open func stopLoading() {}
 }
