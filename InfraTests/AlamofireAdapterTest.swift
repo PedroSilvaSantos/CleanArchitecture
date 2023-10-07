f87a6da69a085831944ff192076f4eda7185ed89
Pedro Silva <pedro.dossantos@castgroup.com.br>
2023-10-07 10:24:35 -0300

    add pod Alamofire in project

--- InfraTests/AlamofireAdapterTest.swift
+++ InfraTests/AlamofireAdapterTest.swift
@@ -1,9 +1,49 @@
 import XCTest
+import Alamofire
 
 final class AlamofireAdapterTest: XCTestCase {
 
-    func test_() {
+    
+    class AlamofireAdapter {
         
+        private var session: Session
+        
+        init(session: Session = .default) {
+            self.session = session
+        }
+        
+        func post(to url: URL) {
+           
+        }
+    }
+    
+    func test_() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = Session(configuration: configuration) //criando uma session local para pode interceptar as requisicoes
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: url)
     }
 
+}
+
+
+//class URLProtocol
+//validar o alamofire com os metodos abaixo
+class URLProtocolStub: URLProtocol {
+    open override class func canInit(with request: URLRequest) -> Bool {
+        //true - quero interceptar todas as requisicoes
+        return true
+    }
+    
+    open override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    open override func startLoading() {
+        
+    }
+    
+    open override func stopLoading() {}
 }
---
+++ Podfile
@@ -0,0 +1,35 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '13.0'
+
+target 'Data' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Data
+
+  target 'DataTests' do
+    # Pods for testing
+  end
+
+end
+
+target 'Domain' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Domain
+
+end
+
+target 'Infra' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Infra
+	pod 'SwiftyJSON', '~> 2.3'
+
+  target 'InfraTests' do
+    # Pods for testing
+  end
+
+end
