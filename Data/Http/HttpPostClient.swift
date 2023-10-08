7ae4601434b097ce286f23cb19eaddac4b251f93
Pedro Silva <pedro.dossantos@castgroup.com.br>
2023-10-08 12:11:59 -0300

    add CI scheme

--- Data/Http/HttpPostClient.swift
+++ Data/Http/HttpPostClient.swift
@@ -2,10 +2,6 @@ import Foundation
 
 //Principios do SOLID
 //S - Interface segregation
-public protocol HttpGetClient {
-    func get(url: URL)
-}
-
 public protocol HttpPostClient {
     func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?,HttpError>) -> Void)
 }
